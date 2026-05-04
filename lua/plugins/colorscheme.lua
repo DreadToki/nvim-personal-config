7d97a44aeee491962e6cb59532ee62a91927dc26
DreadToki <chillfateofficial@gmail.com>
2026-05-04 10:12:36 +0300

    Initial LazyVim config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "latte", -- Це саме та світла версія
+    },
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = { variant = "dawn" },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    opts = {
+      theme = "lotus",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
