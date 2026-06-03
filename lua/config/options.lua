3f5f7eb687ec757d92ef8e4148c9ee313ed1123b
DreadToki <chillfateofficial@gmail.com>
2026-06-04 01:52:49 +0300

    fix: Format XML/AXAML formats

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- -- ~/.config/nvim/lua/config/options.lua
+
+vim.filetype.add({
+  extension = {
+    axaml = "xml",
+  },
+})
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,6 +3,22 @@ return {
   opts = {
     formatters_by_ft = {
       cs = { "csharpier" },
+      xml = { "prettier" },
+      axaml = { "prettier" },
+    },
+
+    formatters = {
+      prettier = {
+        prepend_args = {
+          "--parser",
+          "xml",
+          -- Pointing directly to the plugin.js file as demanded by Node's ESM resolution
+          "--plugin",
+          vim.fn.expand("~/.npm-global/lib/node_modules/@prettier/plugin-xml/src/plugin.js"),
+          "--print-width",
+          "120",
+        },
+      },
     },
   },
 }
